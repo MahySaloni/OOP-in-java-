96848ceece2140b140d9945bc81a1b990a70bb7e
Saloni Mahyavanshi <smahyavanshi@binghamton.edu>
2025-07-21 16:35:17 +0400

    constructing a class

---
+++ class/BooksMenu.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+public class BooksMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        book[] books = new book[10];
+        int bkIdx = 0;
+
+        while (true) {
+            System.out.println("Press 1 to view books, 2 to add books, any other key to exit");
+            String userAction = scanner.nextLine();
+import java.util.Scanner;
+
+public class BooksMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        book[] books = new book[10];
+        int bkIdx = 0;
+
+        while (true) {
+            System.out.println("Press 1 to view books, 2 to add books, any other key to exit");
+            String userAction = scanner.nextLine();
+
+            if (userAction.equals("1")) {
+                for (int i = 0; i < books.length; i++) {
+                    if (books[i] != null) {
+                        System.out.println(books[i]);
+                    }
+                }
+            } else if (userAction.equals("2")) {
+                if (bkIdx == 10) {
+                    System.out.println("10 books added already. Cannot add any more books!");
+                    continue;
+                }
+
+                System.out.println("Enter book title");
+                String tmpTitle = scanner.nextLine();
+                System.out.println("Enter book author");
+                String tmpAuthor = scanner.nextLine();
+                System.out.println("Enter book price");
+                float tmpPrice = Float.parseFloat(scanner.nextLine());
+
+                book bkTmp = new book();
+                bkTmp.setTitle(tmpTitle);
+                bkTmp.setAuthor(tmpAuthor);
+                bkTmp.setPrice(tmpPrice);
+                books[bkIdx++] = bkTmp;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+            if (userAction.equals("1")) {
+                for (int i = 0; i < books.length; i++) {
+                    if (books[i] != null) {
+                        System.out.println(books[i]);
+                    }
+                }
+            } else if (userAction.equals("2")) {
+                if (bkIdx == 10) {
+                    System.out.println("10 books added already. Cannot add any more books!");
+                    continue;
+                }
+
+                System.out.println("Enter book title");
+                String tmpTitle = scanner.nextLine();
+                System.out.println("Enter book author");
+                String tmpAuthor = scanner.nextLine();
+                System.out.println("Enter book price");
+                float tmpPrice = Float.parseFloat(scanner.nextLine());
+
+                book bkTmp = new book();
+                bkTmp.setTitle(tmpTitle);
+                bkTmp.setAuthor(tmpAuthor);
+                bkTmp.setPrice(tmpPrice);
+                books[bkIdx++] = bkTmp;
+            } else {
+                break;
+            }
+        }
+    }
+}
