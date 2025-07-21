ee3d947c653837728ead012aa2174913e93eed63
Saloni Mahyavanshi <smahyavanshi@binghamton.edu>
2025-07-21 13:04:38 +0400

    constructing a class

---
+++ class/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        book myBook = new book();
+        myBook.setTitle("The Alchemist");
+        myBook.setAuthor("Paulo Coelho");
+        myBook.setPrice(19.99f);
+
+        System.out.println(myBook);
+    }
+}
