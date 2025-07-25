0b31a8860efb421e111b39a0fdaca8719bb41934
Saloni Mahyavanshi <smahyavanshi@binghamton.edu>
2025-07-25 17:04:05 +0400

    filereader

---
+++ lists/arraylist/Student.java
@@ -0,0 +1,46 @@
+/* Create a class Student with attributes such as name, age and major. 
+Create a console menu to add, update, delete items in the list and sort the students by age */
+
+public class Student {
+    private String name;
+    private int age;
+    private String major;
+
+    // Constructor
+    public Student(String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    // Override toString() to display student details
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age + ", Major: " + major;
+    }
+}
